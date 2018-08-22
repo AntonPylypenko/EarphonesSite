46d8a18f2ad334fd6b7965e0a119151482e02128
AntonPylypenko <anton.pylypenko5@gmail.com>
2018-08-22 13:23:12 +0300

    Add files via upload

---
+++ earphones/js/custom.js
@@ -0,0 +1,94 @@
+function updater(d, h, m, s) {
+  // День сброса - 27 сентября 2015 года (и далее каждые три дня)
+  var baseTime = new Date(2015, 8, 27);
+  // Период сброса — 3 дня
+  var period = 3*24*60*60*1000;
+
+  function update() {
+    var cur = new Date();
+    // сколько осталось миллисекунд
+    var diff = period - (cur - baseTime) % period;
+    // сколько миллисекунд до конца секунды
+    var millis = diff % 1000;
+    diff = Math.floor(diff/1000);
+    // сколько секунд до конца минуты
+    var sec = diff % 60;
+    if(sec < 10) sec = "0"+sec;
+    diff = Math.floor(diff/60);
+    // сколько минут до конца часа
+    var min = diff % 60;
+    if(min < 10) min = "0"+min;
+    diff = Math.floor(diff/60);
+    // сколько часов до конца дня
+    var hours = diff % 24;
+    if(hours < 10) hours = "0"+hours;
+    var days = Math.floor(diff / 24);
+    d.innerHTML = days;
+    h.innerHTML = hours;
+    m.innerHTML = min;
+    s.innerHTML = sec;
+  
+    // следующий раз вызываем себя, когда закончится текущая секунда
+    setTimeout(update, millis);
+  }
+  setTimeout(update, 0);
+}
+
+updater(document.getElementById("days"),
+ document.getElementById("hours"), document.getElementById("minutes"),
+ document.getElementById("seconds"));
+
+
+
+function initMap() {
+    var coordinates = {lat: 50.435085, lng: 30.533756},
+      coordinatesL = {lat: 49.840981, lng: 24.033199},
+       coordinatesV = {lat: 49.233263, lng: 28.475443},
+     // Координаты центра карты 
+        markerImg = 'images/marker.png', //  Иконка для маркера  
+   
+    // создаем карту и настраеваем 
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 6, // определяет первоначальный масштаб
+        disableDefaultUI: true, // убирает элементы управления
+        scrollwheel: false, // отключает масштабирование колесиком мыши (бывает полезно, если карта на всю ширину страницы и перебивает прокрутку вниз).
+    	
+    });
+
+    // маркер
+    marker = new google.maps.Marker({
+        position: coordinates, // координаты маркера 
+        map: map, //  ставим маркер в карту с id map
+        animation: google.maps.Animation.DROP, // анимация маркера DROP / BOUNCE
+        icon: markerImg,
+    });
+
+     markerL = new google.maps.Marker({
+        position: coordinatesL, // координаты маркера 
+        map: map, //  ставим маркер в карту с id map
+        animation: google.maps.Animation.DROP, // анимация маркера DROP / BOUNCE
+        icon: markerImg,
+    });
+      markerV = new google.maps.Marker({
+        position: coordinatesV, // координаты маркера 
+        map: map, //  ставим маркер в карту с id map
+        animation: google.maps.Animation.DROP, // анимация маркера DROP / BOUNCE
+        icon: markerImg,
+    });
+
+    // Отцентровка карты при ресайзе
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+	});
+
+}
+
+// Запускаем карту при загрузки страницы
+google.maps.event.addDomListener(window, 'load', initMap); 
+
+   
+
+
